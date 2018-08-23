008cad802f7011b44be7677eb13d73836e0cd7c6
lufyluo <lufyl@163.com>
2018-08-23 16:33:11 +0800

    rebuid dubbo project

--- esserver/providers/src/main/java/com/voice/esserver/providers/ProvidersApplication.java
+++ esserver/providers/src/main/java/com/voice/esserver/providers/ProvidersApplication.java
@@ -1,12 +1,10 @@
 package com.voice.esserver.providers;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfiguration
 public class ProvidersApplication {
 
 	public static void main(String[] args) {
